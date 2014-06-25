2fb702e7149f874b1148ff9716b80d72a2286bb2
Dustin Moorman <dustin.moorman@gmail.com>
2014-06-25 11:39:35 -0500

    lambda lunch

--- randomLunch.js
+++ randomLunch.js
@@ -2,7 +2,7 @@
 
 console.dir((function(places){
     return places[Math.floor((Math.random() * places.length) + 0)]
-	})([
+  })([
     'chilosos', 'Shadys', 'burger street',
     'taco ocho', 'jorgs', 'fox and hound', 'bavarian grille',
     'shucks', 'abocas', 'main st bistro'
